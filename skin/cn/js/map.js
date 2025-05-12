ab88048053ddf375659854b70447a130bcc61218
Yuxiang LI <yuxiang.li@shipt.com>
2025-05-12 13:09:48 -0400

    initial commit

---
+++ skin/cn/js/map.js
@@ -0,0 +1,3 @@
+(function() { window.BMap_loadScriptTime = (new Date).getTime();
+    document.write('<script type="text/javascript" src="/template/skin/js/bmap.js"></script>');
+    document.write('<link rel="stylesheet" type="text/css" href="/template/skin/css/bmap.css"/>'); })();
